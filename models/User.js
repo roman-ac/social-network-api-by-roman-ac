4a3ba7ac09a0f2c6ffa5c4f52989b1826f207dda
Roman Ac <roman25xx@gmail.com>
2022-04-10 11:18:05 +1000

    User.js

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+// Schema to create user model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+              // REGEX to validate correct email
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/],
+    },
+    thoughts: [{
+      type: Schema.Types.ObjectID,
+      ref: 'Thought'
+    }],
+    friends: [{
+      type: Schema.Types.ObjectID,
+      ref: 'User'
+    }],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// getting total count of friends
+userSchema
+  .virtual('friendCount')
+  // Getter
+  .get(function () {
+    return this.friends.length;
+  });
+  
+// creating the User model using the User Schema
+const User = model('User', userSchema);
+
+module.exports = User;
